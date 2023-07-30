f3dc4f1c692875d57cedc9bdfbb339a916639871
Ирина Б <koz.io@yandex.ru>
2023-07-30 15:51:02 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://d8752e60-082e-418f-a4e9-b43a749382c4.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
